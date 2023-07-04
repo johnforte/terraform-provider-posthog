05333536d4415e2434894f6f7e5f5018280a59c9
John Forte <fortejohn@gmail.com>
2023-07-04 16:02:41 -0400

    feat: map out json values and convert to terraform types

--- posthog/resource_insight_model.go
+++ posthog/resource_insight_model.go
@@ -2,6 +2,7 @@ package posthog
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 	"io"
 )
@@ -11,10 +12,30 @@ type Insight struct {
 	ShortId     types.String `tfsdk:"short_id"`
 	Name        types.String `tfsdk:"name"`
 	DerivedName types.String `tfsdk:"derived_name"`
+	Filter      types.Map    `tfsdk:"filter"`
+}
+
+type InsightResponse struct {
+	id           int64             `json:"id"`
+	short_id     string            `json:"short_id"`
+	name         string            `json:"name"`
+	derived_name string            `json:"derived_name"`
+	filter       map[string]string `json:"filter"`
 }
 
 func convertResponseToInsight(response io.Reader) Insight {
-	var target Insight
-	json.NewDecoder(response).Decode(&target)
-	return target
+	var profile InsightResponse
+	decoder := json.NewDecoder(response)
+	err := decoder.Decode(&profile)
+	if err != nil {
+		return Insight{}
+	}
+	fmt.Println(profile)
+	return Insight{
+		Id:          types.Int64Value(profile.id),
+		ShortId:     types.StringValue(profile.short_id),
+		Name:        types.StringValue(profile.name),
+		DerivedName: types.StringValue(profile.derived_name),
+		Filter:      types.MapValue(profile.filter),
+	}
 }
