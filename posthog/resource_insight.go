2302fd5237c844a8bad83e0f8d912ff0fbadcf09
John Forte <fortejohn@gmail.com>
2023-07-12 19:58:48 -0400

    fix: typing

--- posthog/resource_insight.go
+++ posthog/resource_insight.go
@@ -9,6 +9,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/mapplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/types"
 	"net/http"
 )
 
@@ -44,6 +45,7 @@ func (r *insightResource) Schema(_ context.Context, req resource.SchemaRequest, 
 				PlanModifiers: []planmodifier.String{stringplanmodifier.RequiresReplace()},
 			},
 			"filter": schema.MapAttribute{
+				ElementType:   types.StringType,
 				Optional:      true,
 				PlanModifiers: []planmodifier.Map{mapplanmodifier.RequiresReplace()},
 			},
